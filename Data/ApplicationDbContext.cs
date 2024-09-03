68bdca6f40ae15551b2d48163ac70dc67678d758
Finnley <dev.finnleygrange@icloud.com>
2024-09-03 09:27:53 +0100

    feat: add dbcontext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceWebsite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
