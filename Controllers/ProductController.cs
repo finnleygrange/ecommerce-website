c4f4f787d06dd76a6571b9375d0ade54b168c3a6
Finnley <dev.finnleygrange@icloud.com>
2024-09-03 13:59:20 +0100

    feat: add product table

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceWebsite.Data;
+using ECommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceWebsite.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _context.Products.ToList();
+            return View(products);
         }
     }
 }
